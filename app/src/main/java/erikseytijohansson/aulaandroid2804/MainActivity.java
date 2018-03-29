e42eb35124dd51eff6c29bc03ca1fbb37d3c62e9
erikseyti <erikseyti@gmail.com>
2018-03-28 21:46:38 -0300

    Adicionado ListView e arrumado bug com nome de imagens

--- app/src/main/java/erikseytijohansson/aulaandroid2804/MainActivity.java
+++ app/src/main/java/erikseytijohansson/aulaandroid2804/MainActivity.java
@@ -4,13 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lisViewTimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lisViewTimes = (ListView)findViewById(R.id.listTimes);
+
+        String times[] = {"Flamengo","ACP","Internacional","Avai"};
+
+        ArrayAdapter<String> adapterTimes =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,times);
+
+        lisViewTimes.setAdapter(adapterTimes);
     }
 
     public void chamarSegundaTela(View view)
