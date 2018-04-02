37fdb995a0b86be7d2ea485977bcbf6c5f6d4fc6
erikseyti <erikseyti@gmail.com>
2018-04-02 20:03:17 -0300

    adicionado um comentario ao projeto

--- Aula2803-master/app/src/main/java/com/example/aluno/aula2803/Time.java
+++ Aula2803-master/app/src/main/java/com/example/aluno/aula2803/Time.java
@@ -8,6 +8,8 @@ public class Time {
     private long id;
     private String nome;
     private String cidade;
+    // esta o valor inteiro pq o objeto drawable puxa uma instancia do elemento R que busca o valor dentro do listview que aparece como inteiro o valor
+    // valor apenas para exibição (normalmente a imagem estará ou na galeria do celular ou no bdd da aplicação)
     private int imagem;
 
     public Time(long id, String nome, String cidade,
